1f4fcecdea593633bad629a6267c85ffe09f1ef8
ssdivya08 <ssaidivya08@gmail.com>
2026-01-14 13:04:18 +0530

    Update script.js

--- Task1_Portfolio/script.js
+++ Task1_Portfolio/script.js
@@ -61,3 +61,19 @@ if (contactBtn) {
     console.log("User clicked Contact Me");
   });
 }
+
+// Back to Top button logic
+window.addEventListener("scroll", () => {
+  const btn = document.getElementById("backToTop");
+  if (btn) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
